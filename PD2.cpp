3afc8d9ec3832632cc0e37c094b99245fff71400
Siddhant borase <69421219+SIDDHANTBORASE@users.noreply.github.com>
2025-09-25 12:15:02 +0530

    Update PD2.cpp

--- PD2.cpp
+++ PD2.cpp
@@ -1,41 +1,76 @@
 #include <iostream>
 using namespace std;
 
-class SmartLight{
+class University{
+private:
+	string Uname ="Pune University";
+	string location= "Pune";
+
 public:
-	void LightFeature(){
-	  cout<<"Smart Light Turning On the Lights"<<endl;
+     virtual void Getdata(){
+	cout<<"Enter the University Name: " ;
+	getline(cin,Uname);
+	cout<<"Enter the Location of University: " ;
+	getline(cin, location);
+    cout<<"\n"<<endl;
 }
+    virtual void displayInfo(){  	
+       	 	cout << "Name of the University: " << Uname << endl;
+		cout<< "Location of University: " <<location<<endl;   
+ }
 };
 
-class SmartFan{
-public:
-	void FanFeature(){
-	  cout<<"Smart Fan is Turning On" <<endl;
-}
-};
-
-class SmartAC{
-public:
-	void AcFeature(){
-	  cout<<"Ac is Turning On "<<endl;
-}
-};
-
-class SmartPhone:public SmartLight,public SmartFan,public SmartAC{
-public:
-	void DisplayFeatures(){
-	  cout<<"Features of the Phone: "<<endl;
-	  SmartLight::LightFeature();
-	  SmartFan::FanFeature();
-	  SmartAC::AcFeature();	
-}
-};
-
-
-int main(){
-	SmartPhone phone;
-	phone.DisplayFeatures();
-	return 0;
+class College : public University {
+private:
+    string Collagename = "Matoshri College Of Engineering: " ;
+    string Principle_Name ;
 	
+
+public:
+
+     void Getdata() override{
+    cout<<"\n"<<endl;
+    cout<<"College Name :"<<Collagename<<endl;
+	cout<<"Enter Name of Principle: ";
+	getline(cin,Principle_Name);
+}
+    void displayInfo() override {
+         	cout << "Name of the College: " << Collagename << endl;
+		cout<<"Principle Name: " <<Principle_Name <<endl;    
+}
+};
+
+class Department : public College {
+private: 
+	string DepartmentName;
+	int departmentstaffcount;
+	int departmentclassrooms;
+public:
+
+	void Getdata() override{
+	University::Getdata();
+	College::Getdata();
+	cout<<"Enter the Department Name " ;
+	getline(cin,DepartmentName);
+	cout<<"Enter the Staff Count of Department:" ;
+	cin>>departmentstaffcount;
+	cout<<"Enter the Classrooms Present in The Department: ";
+	cin>>departmentclassrooms;
+}
+	void displayInfo() override {
+	cout << endl<<endl;
+    cout << "Name of the Department: " << DepartmentName<< endl;
+	cout<<"Staff Count of Department:" <<departmentstaffcount <<endl ;
+	cout<<"Enter the Classrooms Present in The Department: " <<departmentclassrooms<<endl;
+	College::displayInfo();	
+	University::displayInfo();
+		
+    	}	
+};
+
+int main() {
+    Department c;
+    c.Getdata();
+    c.displayInfo();
+    return 0;
 }
