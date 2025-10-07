9737ef7e26e497e6111e36f2fab1b31a46fa6541
Siddhant borase <69421219+SIDDHANTBORASE@users.noreply.github.com>
2025-10-07 10:00:14 +0530

    Update PD2.cpp

--- PD2.cpp
+++ PD2.cpp
@@ -1,76 +1,60 @@
 #include <iostream>
 using namespace std;
 
-class University{
-private:
-	string Uname ="Pune University";
-	string location= "Pune";
-
+class SmartDevice {
 public:
-     virtual void Getdata(){
-	cout<<"Enter the University Name: " ;
-	getline(cin,Uname);
-	cout<<"Enter the Location of University: " ;
-	getline(cin, location);
-    cout<<"\n"<<endl;
-}
-    virtual void displayInfo(){  	
-       	 	cout << "Name of the University: " << Uname << endl;
-		cout<< "Location of University: " <<location<<endl;   
- }
+	int power;
+   	virtual void DisplayFeatures() {
+       cout<<"Smart Devices power: "<<power;
+    }
+
+};
+class SmartLight:public SmartDevice {
+public:
+    void DisplayFeatures(int op) overrides {
+     	SmartDevice::DisplayFeatures();
+     	if (power == 0){
+     		cout <<"Cant turn On Light."<<endl;
+		 	return;
+		 }
+		if(op != 0){
+		cout << "Smart Light Turning On the Lights" << endl;
+    
+		}
+    }
 };
 
-class College : public University {
-private:
-    string Collagename = "Matoshri College Of Engineering: " ;
-    string Principle_Name ;
-	
-
+class SmartFan {
 public:
-
-     void Getdata() override{
-    cout<<"\n"<<endl;
-    cout<<"College Name :"<<Collagename<<endl;
-	cout<<"Enter Name of Principle: ";
-	getline(cin,Principle_Name);
-}
-    void displayInfo() override {
-         	cout << "Name of the College: " << Collagename << endl;
-		cout<<"Principle Name: " <<Principle_Name <<endl;    
-}
+     void DisplayFeatures() overrides {
+     	SmartDevice::DisplayFeatures();
+        cout << "Smart Fan is Turning On" << endl;
+    }
 };
 
-class Department : public College {
-private: 
-	string DepartmentName;
-	int departmentstaffcount;
-	int departmentclassrooms;
+class SmartAC {
 public:
-
-	void Getdata() override{
-	University::Getdata();
-	College::Getdata();
-	cout<<"Enter the Department Name " ;
-	getline(cin,DepartmentName);
-	cout<<"Enter the Staff Count of Department:" ;
-	cin>>departmentstaffcount;
-	cout<<"Enter the Classrooms Present in The Department: ";
-	cin>>departmentclassrooms;
-}
-	void displayInfo() override {
-	cout << endl<<endl;
-    cout << "Name of the Department: " << DepartmentName<< endl;
-	cout<<"Staff Count of Department:" <<departmentstaffcount <<endl ;
-	cout<<"Enter the Classrooms Present in The Department: " <<departmentclassrooms<<endl;
-	College::displayInfo();	
-	University::displayInfo();
-		
-    	}	
+     void DisplayFeatures() overrides {
+     	SmartDevice::DisplayFeatures();
+        cout << "Ac is Turning On" << endl;
+    }
 };
+
+
 
 int main() {
-    Department c;
-    c.Getdata();
-    c.displayInfo();
+	int choices;
+	cout<<"Function of SmartDevices: ";
+	do{	
+	cout<<"1. Turn on/off light \n2. Turn on/off Ac \n3. Turn on/off Fan \n4. Exit Room." <<endl;
+	cin>>choices;
+	switch(choices)
+    SmartLight light;
+    SmartFan Fan;
+    SmartAC ac;
+    light.DisplayFeatures();
+	Fan.DisplayFeatures();
+	Ac.DisplayFeatures();
+}
     return 0;
 }
